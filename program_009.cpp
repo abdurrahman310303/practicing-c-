38ee8e3c80f09ab0280fec7b7d7466a7fafd906d
abdurrahman310303 <abdurrahman310303@gmail.com>
2023-01-04 17:45:00 +0500

    while loop counter program

---
+++ program_009.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int count = 0;
+    while (count < 5) {
+        cout << "Count is: " << count << endl;
+        count++;
+    }
+    return 0;
+}
