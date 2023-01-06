43c9e5223a4265e26d206486098d980e46cb4ba2
abdurrahman310303 <abdurrahman310303@gmail.com>
2023-01-06 14:15:00 +0500

    character case checker

---
+++ program_013.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch;
+    cout << "Enter a character: ";
+    cin >> ch;
+    if (ch >= 'a' && ch <= 'z') {
+        cout << ch << " is lowercase" << endl;
+    } else if (ch >= 'A' && ch <= 'Z') {
+        cout << ch << " is uppercase" << endl;
+    } else {
+        cout << ch << " is not a letter" << endl;
+    }
+    return 0;
+}
