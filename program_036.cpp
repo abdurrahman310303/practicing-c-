fd8b6a3e8c4768986bfac7aa3816ab285eea8faf
abdurrahman310303 <abdurrahman310303@gmail.com>
2023-01-13 16:20:00 +0500

    prime check using function

---
+++ program_036.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int num) {
+    if (num <= 1) return false;
+    for (int i = 2; i <= num / 2; i++) {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
+
+int main() {
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+    if (isPrime(number)) {
+        cout << number << " is prime" << endl;
+    } else {
+        cout << number << " is not prime" << endl;
+    }
+    return 0;
+}
