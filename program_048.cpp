0a828159bacf3d877005ebb2c7afebde191e1a35
abdurrahman310303 <abdurrahman310303@gmail.com>
2023-01-18 17:30:00 +0500

    matrix addition program

---
+++ program_048.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int matrix1[2][2], matrix2[2][2], sum[2][2];
+    cout << "Enter first 2x2 matrix:" << endl;
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            cin >> matrix1[i][j];
+        }
+    }
+    cout << "Enter second 2x2 matrix:" << endl;
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            cin >> matrix2[i][j];
+        }
+    }
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            sum[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
+    cout << "Sum of matrices:" << endl;
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            cout << sum[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
