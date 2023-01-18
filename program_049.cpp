8a687725de31937a63b2545eedad1c697c8f7329
abdurrahman310303 <abdurrahman310303@gmail.com>
2023-01-18 20:45:00 +0500

    simple interest calculator

---
+++ program_049.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float principal, rate, time, interest;
+    cout << "Enter principal amount: ";
+    cin >> principal;
+    cout << "Enter rate of interest: ";
+    cin >> rate;
+    cout << "Enter time in years: ";
+    cin >> time;
+    interest = (principal * rate * time) / 100;
+    cout << "Simple Interest: " << interest << endl;
+    cout << "Total Amount: " << principal + interest << endl;
+    return 0;
+}
