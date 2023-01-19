0087146e9a353326e5dbe20e78cf12e5a61c78be
abdurrahman310303 <abdurrahman310303@gmail.com>
2023-01-19 09:45:00 +0500

    calendar days calculator

---
+++ calendar_days.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int year, month, days;
+    cout << "Enter year: ";
+    cin >> year;
+    cout << "Enter month (1-12): ";
+    cin >> month;
+    
+    if (month == 2) {
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            days = 29;
+        } else {
+            days = 28;
+        }
+    } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+        days = 30;
+    } else {
+        days = 31;
+    }
+    
+    cout << "Number of days: " << days << endl;
+    return 0;
+}
